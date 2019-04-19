4540cec46172edc4b6813aee92f134f419c7cfec
Quentin <Cavle21@users.noreply.github.com>
2019-04-19 18:13:34 -0400

    Update EventHandler.ps1

--- EventHandler.ps1
+++ EventHandler.ps1
@@ -1,3 +1,21 @@
 class EventHandler {
-    
+
+    RegisterEvent([string]$sourceIdentifier){
+        Register-EngineEvent -SourceIdentifier $sourceIdentifier -Action {
+            Write-host $_.messageData
+        }
+    }
+
+    NewEvent ([string]$sourceIdentifier, [string]$messageData, [string]$sendingObjectName){
+        New-Event -SourceIdentifier $sourceIdentifier -Sender $sendingObjectName -MessageData $messageData
+    }
+
+    ListEvents (){
+        Write-host Get-Event | Select-Object SourceIdentifier, MessageData, SendingObject
+    }
+
+    RegisterForwardingEvent([string]$sourceIdentifier){
+        Register-EngineEvent -SourceIdentifier $sourceIdentifier -Forward
+    }
+
 }
